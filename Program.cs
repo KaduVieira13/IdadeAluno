c0c3c105b7e904f00360567203ecad4eb854b230
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-03-25 12:17:49 -0300

    Finalizando nascimento e aniversário de 18 anos

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IdadeAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataDigitada;
+
+            Console.WriteLine("---Data de aniversário--- \n");
+            Console.WriteLine("Digite a data do seu nascimento no formato: AAAA,MM.DD");
+            dataDigitada = Console.ReadLine();
+            DateTime nascimentoUsuario = Convert.ToDateTime(dataDigitada);
+            DateTime aniversarioDezoito = nascimentoUsuario.AddYears(18);
+            Console.WriteLine($"O dia do seu nascimento foi: {nascimentoUsuario.ToLongDateString()}");
+            Console.WriteLine($"Seu aniversário de 18 anos: {aniversarioDezoito.ToLongDateString()}");
+
+          
+        }
+    }
+}
